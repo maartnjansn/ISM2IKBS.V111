f7a9a4a234c8f66c5f2383750558d68467d57824
Thomas Baart <m.thomasbaart@gmail.com>
2012-04-09 23:02:22 +0200

    Added: Lege Main class

---
+++ ISM2IKBSV111/src/ism2ikbsv111/Main.java
@@ -0,0 +1,15 @@
+package ism2ikbsv111;
+
+/**
+ * 
+ * @author Maarten Jansen
+ * @author Ido Bosman
+ * @author Thomas Baart
+ * @author Jormen Janssen
+ * @author Gerjan Kerssies
+ */
+public class Main {
+   public static void main(String[] args) {
+     // TODO: Application logic here
+   }
+}
